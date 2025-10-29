f7d7bd344da8cecee888678cf0a322c22df0983f
Steve Hirschhorn <stevehirschhorn@Steves-MacBook-Pro.local>
2025-10-29 13:23:19 -0600

    Fix BLS API import in DataUpdater

--- src/services/dataUpdater.js
+++ src/services/dataUpdater.js
@@ -2,7 +2,7 @@
 import fs from 'fs';
 import path from 'path';
 import { fileURLToPath } from 'url';
-import { BLSApiService } from './blsApi.js';
+import BLSApiService from './blsApi.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
